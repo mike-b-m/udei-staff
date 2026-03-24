a9607bf6b63834a2d21358d6c990165e08accd74
mike-b-m <mikebarrymichel@gmail.com>
2026-03-24 14:59:14 -0400

    update

--- app/component/download_PDF/download_pdf.tsx
+++ app/component/download_PDF/download_pdf.tsx
@@ -1,28 +1,28 @@
-"use client";
+// "use client";
 
-import html2pdf from "html2pdf.js";
+// import html2pdf from "html2pdf.js";
 
-export default function DownloadPDFButton() {
-  const handleDownload = () => {
-    const element: any = document.getElementById("student-infos" );
+// export default function DownloadPDFButton() {
+//   const handleDownload = () => {
+//     const element: any = document.getElementById("student-infos" );
 
-    const opt: any = {
-      margin: 10,
-      filename: "user-profile.pdf",
-      image: { type: "jpeg", quality: 0.98 },
-      html2canvas: { scale: 2 }, // improves quality
-      jsPDF: { unit: "mm", format: "a4", orientation: "portrait" },
-    };
+//     const opt: any = {
+//       margin: 10,
+//       filename: "user-profile.pdf",
+//       image: { type: "jpeg", quality: 0.98 },
+//       html2canvas: { scale: 2 }, // improves quality
+//       jsPDF: { unit: "mm", format: "a4", orientation: "portrait" },
+//     };
 
-    html2pdf().set(opt).from(element).save();
-  };
+//     html2pdf().set(opt).from(element).save();
+//   };
 
-  return (
-    <button
-      onClick={handleDownload}
-      className="mt-4 px-4 py-2 bg-blue-600 text-white rounded-lg"
-    >
-      Download PDF
-    </button>
-  );
-}
+//   return (
+//     <button
+//       onClick={handleDownload}
+//       className="mt-4 px-4 py-2 bg-blue-600 text-white rounded-lg"
+//     >
+//       Download PDF
+//     </button>
+//   );
+// }
